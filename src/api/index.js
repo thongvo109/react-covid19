2a40d16e68e10e1624fbe1f393af106a9a208280
thongvm <thongvo109@gmail.com>
2020-05-12 00:00:08 +0700

    frist commit

---
+++ src/api/index.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const url = " https://covid19.mathdro.id/api";
+
+export const fectchData = async () => {
+  try {
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(url);
+    const modifiedData = {
+      confirmed: confirmed,
+      recovered: recovered,
+      deaths: deaths,
+      lastUpdate: lastUpdate,
+    };
+    return modifiedData;
+  } catch (error) {}
+};
---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ConntryPicker = () => {
+    return <h1> ConntryPicker </h1>;
+};
+
+export default ConntryPicker;
